61b8bc58f22448a6482c2b3fc7b0c6bd68ea623e
emi4218 <emi4218@gmail.com>
2021-09-13 12:23:43 +0200

    test affichage 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
 
     let testPostIt1 = new PostIt(200, 200, 300, 300, "green", "Test test fonctionne s'il te plaît");
-    testPostIt1.afficher();
+
 
 })
+testPostIt1.afficher();
