85346a7fe9a92c4f30502be8327ec87464659a27
emi4218 <emi4218@gmail.com>
2021-09-13 12:18:39 +0200

    chagement couleur

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
 
-    let testPostIt1 = new PostIt(200, 200, 300, 300, "chartreuse", "Test test fonctionne s'il te plaît");
+    let testPostIt1 = new PostIt(200, 200, 300, 300, "green", "Test test fonctionne s'il te plaît");
     testPostIt1.afficher();
 
 })
