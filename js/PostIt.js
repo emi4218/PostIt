49a94b9b275e5be3543df67f8c27e5b0b9ae8e82
emi4218 <emi4218@gmail.com>
2021-09-13 11:44:37 +0200

    test 1

--- js/PostIt.js
+++ js/PostIt.js
@@ -1,4 +1,26 @@
 class PostIt {
 
+    x;
+    y;
+    largeur;
+    hauteur;
+    couleur;
+    texte;
 
+    constructor(x, y, largeur, hauteur, couleur, texte) {
+        this.x = x;
+        this.y = y;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.couleur = couleur;
+        this.texte = texte;
+    }
+
+
+    affiche() {
+        let monPostIt = document.createElement('div');
+        monPostIt.classList.add('postIt');
+        monPostIt.style.gridArea = this.x + "/" + this.y;
+        document.appendChild(monPostIt);
+    }
 }
---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+
+    let monPostIt = new PostIt(200, 200, 300, 300, "blue", "coucou");
+    monPostIt.affiche();
+
+
+})
