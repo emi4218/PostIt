23661165f44fa858faa74a46facd11423ff8cc4b
emi4218 <emi4218@gmail.com>
2021-09-14 21:04:54 +0200

    mise des icônes & autres trucs 14/09/21

--- js/PostIt.js
+++ js/PostIt.js
@@ -8,21 +8,21 @@ class PostIt {
     texte;
     monPostIt;
 
-    constructor(plateau, x, y, largeur, hauteur, couleur, texte) {
-
+    constructor(plateau, num, x = 100, y = 100, largeur = 200, hauteur = 200, couleur = "#46e2ac", texte = "") {
         this.plateau = plateau;
+        this.num = num; // id du post-it
         this.x = x;
         this.y = y;
         this.largeur = largeur;
         this.hauteur = hauteur;
         this.couleur = couleur;
         this.texte = texte;
-        this.monPostIt = document.createElement('div');
+        this.monPostIt = document.createElement('div'); // on créé la div
     }
 
 
     afficher() {
-        this.monPostIt.classList.add('postIt');
+        this.monPostIt.classList.add('postIt'); // on met la classe postIt
         this.monPostIt.style.position = "fixed";
         this.monPostIt.style.top = this.y + "px";
         this.monPostIt.style.left = this.x + "px";
@@ -30,7 +30,29 @@ class PostIt {
         this.monPostIt.style.height = this.hauteur + "px";
         this.monPostIt.style.backgroundColor = this.couleur;
         this.monPostIt.innerHTML = this.texte;
-        this.plateau.appendChild(this.monPostIt);
+        let menu = document.createElement('div');
+        menu.classList.add('bottomMenu');
+        this.monPostIt.appendChild(menu);
+        let resize = document.createElement('i');
+        resize.classList.add('fas', 'fa-expand-alt');
+        menu.appendChild(resize);
+        let edit = document.createElement('i');
+        edit.classList.add('far', 'fa-edit');
+        menu.appendChild(edit);
+        let move = document.createElement('i');
+        move.classList.add('fas', 'fa-hand-pointer');
+        menu.appendChild(move);
+        move.addEventListener('click', event => { // on met l'event click sur move (en gros, qd on clique sur l'icone hand-pointer)
+            console.log("on a cliqué sur le post it " + this.num)
+            if (numPostIt != this.num) { // si le numPostIt (déclaré à -1 dans l'index) est différent du numéro de post-it cliqué
+                numPostIt = this.num; // numPostIt prend le numéro de post-it cliqué
+            }
+            else { // on relache le post it (puisqu'au reclick il n'est plus à -1 mais a pris la valeur du numéro du post-it)
+                numPostIt = -1;
+            }
+            event.stopPropagation(); // pour éviter d'envoyer l'event au post-it et à la page, juste le laisser sur le curseur move
+        })
+        this.plateau.appendChild(this.monPostIt); // on ajoute la div au plateau
     }
 
     deplacer(x, y) {
@@ -38,7 +60,6 @@ class PostIt {
         this.y = y;
         this.monPostIt.style.top = this.y + "px";
         this.monPostIt.style.left = this.x + "px";
-
     }
 
     agrandir(largeur, hauteur) {
--- js/index.js
+++ js/index.js
@@ -1,14 +1,38 @@
+let numPostIt = -1;
+let tabPostIt = [];
+// let action = "";
+
 window.addEventListener('load', () => {
     let plateau = document.getElementById('contain');
 
-    let testPostIt1 = new PostIt(plateau, 200, 200, 300, 300, "#46e2ac", "Test test fonctionne s'il te plaît");
-    testPostIt1.afficher();
+    // let testPostIt1 = new PostIt(plateau, 1, 300, 300, 300, 300, "#46e2ac", "Test test fonctionne s'il te plaît");
+    // testPostIt1.afficher();
 
-    let testPostIt2 = new PostIt(plateau, 600, 600, 200, 200, "#e2f26a", "test 2");
-    testPostIt2.afficher();
-    testPostIt2.deplacer(500, 600);
+    // let testPostIt2 = new PostIt(plateau, 2, 600, 600, 200, 200, "#e2f26a", "test 2");
+    // testPostIt2.afficher();
+    // testPostIt2.deplacer(500, 600);
 
-    testPostIt1.agrandir(400, 400);
-    testPostIt2.changerTexte("Allez, je crois en toi !");
+    // testPostIt1.agrandir(400, 400);
+    // testPostIt2.changerTexte("Allez, je crois en toi !");
+
+
+    document.addEventListener("mousemove", souris)
+    function souris(event) {
+        // console.log(event);
+        if (numPostIt != -1) { // si le post-it existe (différent de -1), on le déplace selon les coordonnées (clientx et y de la souris)
+            tabPostIt[numPostIt].deplacer(event.clientX, event.clientY);
+        }
+    }
+
+    document.getElementById('stockage').addEventListener('click', event => {
+        console.log('ajout post-it')
+        tabPostIt.push(new PostIt(plateau, tabPostIt.length));
+        tabPostIt[tabPostIt.length - 1].afficher(plateau);
+    })
+
+    document.addEventListener('click', () => { // pour pouvoir cliquer à nouveau quand on a déplacé le post-it
+        numPostIt = -1;
+    })
+
 
 })
